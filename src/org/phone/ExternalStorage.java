684b4d63c1d02b44fcbbbd15161bed8ffb48433c
kavyaneeraj <kavyagoalla26@gmail.com>
2021-08-24 14:54:26 +0530

    extSize

--- src/org/phone/ExternalStorage.java
+++ src/org/phone/ExternalStorage.java
@@ -1,7 +1,11 @@
 package org.phone;
 
 public class ExternalStorage {
-	public void size() {
+	public void extSize() {
 		System.out.println("External Size: 128GB");
+	}
+	public static void main(String[] args) {
+		ExternalStorage s =new ExternalStorage();
+		s.extSize();
 	}
 }
