366c4057c7350a2ef2501ebb0163117528c96f85
kavyaneeraj <kavyagoalla26@gmail.com>
2021-08-24 14:45:34 +0530

    size done

--- src/org/phone/ExternalStorage.java
+++ src/org/phone/ExternalStorage.java
@@ -4,4 +4,8 @@ public class ExternalStorage {
 	public void size() {
 		System.out.println("External Size: 128GB");
 	}
+	public static void main(String[] args) {
+		ExternalStorage s =new ExternalStorage();
+		s.size();
+	}
 }
